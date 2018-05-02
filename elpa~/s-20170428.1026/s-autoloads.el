37adcc8c816ac31d7841e20b6e4fe0748f9ab816
lishaobo <lishaobo@zeju.com>
2018-05-02 23:19:35 +0800

    初始化改目录，重新学习emacs
    Signed-off-by: lishaobo <lishaobo@zeju.com>

--- elpa~/s-20170428.1026/s-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; s-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("s.el") (22865 14394 511008 164000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; s-autoloads.el ends here

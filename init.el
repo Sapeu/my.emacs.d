4185938149b7be2f2969f3512c4387cfe972c1ee
lishaobo <lishaobo@zeju.com>
2018-05-06 00:19:50 +0800

    拆分custom.el
    Signed-off-by: lishaobo <lishaobo@zeju.com>

--- init.el
+++ init.el
@@ -31,20 +31,4 @@
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-idle-delay 0.08)
- '(company-minimum-prefix-length 1)
- '(custom-safe-themes
-   (quote
-    ("cc60d17db31a53adf93ec6fad5a9cfff6e177664994a52346f81f62840fe8e23" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(js2-external-variable ((t (:foreground "dark gray")))))
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))

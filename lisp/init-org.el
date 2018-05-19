353b6e76a354753af9ae14ba3be5d50120ba225d
Sapeu <sapeu.lee@foxmail.com>
2018-05-20 00:21:02 +0800

    在日常生活中使用org文档的配置
    Signed-off-by: Sapeu <sapeu.lee@foxmail.com>

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,10 +1,25 @@
 
-(require 'org)
-;; 设置org-mode中src代码高亮
-(setq org-src-fontify-natively t)
+(with-eval-after-load 'org
+  
+  ;; 设置org-mode中src代码高亮
+  (setq org-src-fontify-natively t)
 
-;; 设置org-mode
-(setq org-agenda-files '("~/org"))
-(global-set-key (kbd "C-c a") 'org-agenda)
+  ;; 设置org-mode
+  (setq org-agenda-files '("~/.emacs.d/"))
+  (global-set-key (kbd "C-c a") 'org-agenda)
+
+  (setq org-capture-templates
+	'(("t" "Todo" entry (file+headline "~/.emacs.d/gtd.org" "学习安排")
+	   "* TODO [#B] %?\n  %i\n"
+	   :empty-lines 1)
+	  ("x" "emacs学习的地址" entry (file+headline "~/.emacs.d/gtd.org" "学习安排")
+	   "* TODO [#B] %?\n %(paul/retrieve-chrome-current-tab-url)\n  %i\n %U"
+	   :empty-lines 1))
+	)
+  )
+
+(global-set-key (kbd "C-c r") 'org-capture)
+
+(require 'org-pomodoro)
 
 (provide 'init-org)
--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -22,6 +22,7 @@
 			js2-refactor
 			expand-region
 			iedit
+			org-pomodoro
 			) "Default packages")
 
 

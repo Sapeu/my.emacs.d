e93a452507a5268479668e17155572c4e684779d
Sapeu <sapeu.lee@foxmail.com>
2018-05-26 11:01:40 +0800

    启动错误
    Signed-off-by: Sapeu <sapeu.lee@foxmail.com>

---
+++ .cask/25.3/elpa/f-20180106.122/f-autoloads.el
@@ -0,0 +1,15 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("f.el") (23304 52505 344146 184000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; f-autoloads.el ends here

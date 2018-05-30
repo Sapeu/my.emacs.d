60988e57068f3ca1d667f2e1a5c9ac2248a4654f
Sapeu <sapeu.lee@foxmail.com>
2018-05-30 15:35:38 +0800

    将.spacemacs文件移动到.spacemacs.d/init.el
    Signed-off-by: Sapeu <sapeu.lee@foxmail.com>

--- init.el
+++ init.el
@@ -312,6 +312,8 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (pixel-scroll-mode t)
+  (global-set-key (kbd "<s-return>") 'toggle-frame-fullscreen)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
